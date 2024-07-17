229431c4268fc02bdc7819156f6f12011cd6679c
lucianoschwalm <luciano.rocha21@hotmail.com>
2024-07-17 16:16:55 -0300

    feat: adding find Courses Name route

--- src/routes.ts
+++ src/routes.ts
@@ -9,6 +9,7 @@ router.get("/categories/:id", categoriesController.show);
 //rotas com variavel dinamica deve sempre ficar abaixo das que não a possuem
 router.get("/courses/featured", coursesController.featured);
 router.get("/courses/newest", coursesController.newest);
+router.get("/courses/search", coursesController.search);
 router.get("/courses/:id", coursesController.show);
 
 export { router };
