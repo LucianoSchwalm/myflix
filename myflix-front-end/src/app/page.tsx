5d41aea806d603a46bc2a37e04702b871d11d2b1
lucianoschwalm <luciano.rocha21@hotmail.com>
2024-07-31 20:04:46 -0300

    feat: first commit myflix-front-end

---
+++ myflix-front-end/src/app/page.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+const HomeNotAuth = () => {
+  return (
+    <>
+      <Head>
+        <title>Home</title>
+      </Head>
+      <main></main>
+    </>
+  );
+};
+
+export default HomeNotAuth;
