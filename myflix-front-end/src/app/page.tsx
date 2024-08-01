e5a2070a95307e50a69e5021d4198a3ae7bb9d63
lucianoschwalm <luciano.rocha21@hotmail.com>
2024-08-01 15:54:05 -0300

    fix: fixing metadatas

--- myflix-front-end/src/app/page.tsx
+++ myflix-front-end/src/app/page.tsx
@@ -1,19 +1,9 @@
-import Head from "next/head";
 import styles from "../styles/HomeNoAuth.module.scss";
 
 const HomeNotAuth = () => {
   return (
     <>
-      <Head>
-        <title>Onebitflix</title>
-        <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
-        <meta property="og:title" content="Onebitflix" key="title" />
-        <meta
-          name="description"
-          content="Tenha acesso aos melhores conteúdos de programção de forma simples e fácil!"
-        />
-      </Head>
-      <main></main>
+      <main>Teste xD</main>
     </>
   );
 };
