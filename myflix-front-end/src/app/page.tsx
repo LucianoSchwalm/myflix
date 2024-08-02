998f42374459f2a2b9d5314c4a2d79572c88525b
lucianoschwalm <luciano.rocha21@hotmail.com>
2024-08-02 19:11:55 -0300

    feat: adding card section component

--- myflix-front-end/src/app/page.tsx
+++ myflix-front-end/src/app/page.tsx
@@ -1,6 +1,7 @@
 import styles from "../styles/HomeNoAuth.module.scss";
 import { HeaderNoAuth } from "../components/homeNoAuth/headerNoAuth/headerNoAuth";
 import { PresentationSection } from "../components/homeNoAuth/presentationSection/presentationSection";
+import { CardSection } from "@/components/homeNoAuth/cardSection/cardSection";
 
 const HomeNotAuth = () => {
   return (
@@ -10,6 +11,7 @@ const HomeNotAuth = () => {
           <HeaderNoAuth />
           <PresentationSection />
         </div>
+        <CardSection />
       </main>
     </>
   );
