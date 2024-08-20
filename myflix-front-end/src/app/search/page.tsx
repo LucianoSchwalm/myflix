10a4d09c300f69798df0e8d89fe48dbac1e64730
lucianoschwalm <luciano.rocha21@hotmail.com>
2024-08-20 10:51:24 -0300

    feat:search page style created

--- myflix-front-end/src/app/search/page.tsx
+++ myflix-front-end/src/app/search/page.tsx
@@ -1,16 +1,19 @@
 "use client";
-import styles from "../styles/search.module.scss";
+import styles from "../../styles/search.module.scss";
 import { HeaderAuth } from "../../components/common/HeaderAuth/headerAuth";
 import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import { courseService, CourseType } from "@/services/courseService";
+import { Container } from "reactstrap";
+import { SearchCard } from "@/components/SearchCard/searchCard";
+import { Footer } from "@/components/common/Footer/footer";
 
 const Search = function () {
   const searchParams = useSearchParams();
   const searchName = searchParams.get("name");
   const [searchResult, setSearchResult] = useState<CourseType[]>([]);
 
-  const searchCourses = async function () {
+  const searchCourses = async () => {
     if (typeof searchName === "string") {
       const res = await courseService.getSearch(searchName);
       setSearchResult(res.data.courses);
@@ -22,13 +25,24 @@ const Search = function () {
   }, [searchName]);
 
   return (
-    <main>
-      <HeaderAuth />
-      {searchResult?.map((course) => (
-        <div key={course.id}>
-          <p>{course.name}</p>
+    <main className={styles.main}>
+      <div className={styles.headerFooterBg}>
+        <HeaderAuth />
+      </div>
+      {searchResult.length >= 1 ? (
+        <div className={styles.searchResult}>
+          <Container className="d-flex flex-wrap justify-content-center gap-5 py-4">
+            {searchResult?.map((course) => (
+              <SearchCard key={course.id} course={course} />
+            ))}
+          </Container>
         </div>
-      ))}
+      ) : (
+        <p className={styles.noSearchText}>Nenhum resultado encontrado</p>
+      )}
+      <div className={styles.headerFooterBg}>
+        <Footer />
+      </div>
     </main>
   );
 };
